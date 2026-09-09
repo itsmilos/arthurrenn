eba8229d22e4b964178e3bc3c365024be10a9a7b
skem-skem <lazenda001@gmail.com>
2026-09-09 19:54:07 +0200

    feat: updated sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -18,32 +18,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
 
-    {
-      url: `${siteUrl}/#truth`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-
-    {
-      url: `${siteUrl}/#chapters`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-
-    {
-      url: `${siteUrl}/#reviews`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
-
-    {
-      url: `${siteUrl}/#about`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
   ];
 }
